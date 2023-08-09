0cc3a3069ab3f372d4f466ecea945e00bf781949
dacybernator2 <34093058+dacybernator2@users.noreply.github.com>
2023-08-09 23:08:58 +0100

    Create Main.java
    direct commit

---
+++ Main.java
@@ -0,0 +1,30 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Opt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+
+
+        System.out.println (" CHECK YOUR BMI");
+
+        System.out.println (" ENTER THE NUMERIC VALUE OF YOUR WEIGHT IN KILOGRAM");
+        Scanner input = new Scanner(System.in) ;
+        double weight = input.nextDouble() ;
+
+
+        System.out.println (" ENTER THE NUMERIC VALUE OF YOUR HEIGHT IN METERS");
+        Scanner input2 = new Scanner(System.in) ;
+         double height = input2.nextDouble() ;
+
+
+
+        double bmi = (weight / (height * height));
+
+
+            // Press Ctrl+D to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Cmd+F8.
+            System.out.printf("%.1f",bmi);
+        }
+    }
+
